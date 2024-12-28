d2fecf4a34c9166ccdcb103dc0650a2028785746
mishraabhi09 <abhiimishra1502@gmail.com>
2024-12-28 23:58:48 +0530

    tailwind configuration is done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-2xl font-bold bg-emerald-400">
+       Heyy everyone , this is abhinav and we are now building netflix+gpt.
+    </div>
+  );
+}
+
+export default App;
